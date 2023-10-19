a35d4aa01e56f98665efb0bcbf7dd72e5d51089d
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-20 06:24:30 +0700

    Hoàn thành chức năng click button trong phần bộ lọc và sắp xếp

--- javascript/product.js
+++ javascript/product.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
+    setRateProduct();
+    addActionClickForButtonFilter();
+    addActionClickForButtonSort();
+});
+
+function addActionClickForButtonFilter() {
+    $("div.filter>ul>li>button").click(function (){
+        changeColorButtonCheck($(this));
+    });
+}
+
+function addActionClickForButtonSort() {
+    $("div.sort>ul>li>button").click(function (){
+        changeColorButtonCheck($(this));
+    });
+}
+
+function changeColorButtonCheck(button) {
+   if(button.attr("class").indexOf("checked") >= 0){
+        button.removeClass("checked");
+   }else{
+       button.addClass("checked");
+   }
+}
+
+function setRateProduct(){
     var listStar = $("div.onirvapp--shape-container>ul>li");
     for (var star of listStar) {
         if (star.getAttribute("class") === "checked") {
             star.getElementsByTagName("i")[0].setAttribute("class", "fa-solid fa-star");
         }
     }
-
-    addActionClickForButtonFilter();
-});
-
-function addActionClickForButtonFilter() {
-    var listButton = $("div.filter>ul>li>button");
-    for(var button of listButton){
-        button.click(function () {
-            changeColorButtonCheck($(this));
-        });
-    }
-}
-
-function changeColorButtonCheck(button) {
-   if(key.indexOf("checked") >= 0){
-        button.removeClass("checked");
-   }else{
-       button.addClass("checked");
-   }
 }
