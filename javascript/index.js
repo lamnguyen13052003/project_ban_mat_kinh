602c748faedb4da5dfe986f8e5f7ff556953641f
LamHongPhong <21130473@st.hcmuaf.edu.vn>
2023-10-20 13:43:04 +0700

    hoàn thành  1-5 sectiion của trang index

--- javascript/index.js
+++ javascript/index.js
@@ -23,80 +23,92 @@ function displayPlaceholder(element){
 }
 /**main**/
 /*section 1*/
-// $(document).ready(function () {
-//     $('.right').click(function (event) {
-//         var sldie_sau = $('.active').next();
-//         if(sldie_sau.length != 0){
-//             $('.active').addClass('bien-mat-ben-trai').one('webkitAnimationEnd', function (event) {
-//                 $('.bien-mat-ben-trai').removeClass('bien-mat-ben-trai').removeClass('active');
-//             });
-//             sldie_sau.addClass('active').addClass('di-vao-ben-phai').one('webkitAnimationEnd', function (event) {
-//                 $('.di-vao-ben-phai').removeClass('di-vao-ben-phai');
-//             });
-//         }else {
-//             $('.active').addClass('bien-mat-ben-trai').one('webkitAnimationEnd', function (event) {
-//                 $('.bien-mat-ben-trai').removeClass('bien-mat-ben-trai').removeClass('active');
-//             });
-//             $('.item:first-child').addClass('active').addClass('di-vao-ben-phai').one('webkitAnimationEnd', function (event) {
-//                 $('.di-vao-ben-phai').removeClass('di-vao-ben-phai');
-//             });
-//         }
-//     });
-//
-//     $('.left').click(function (event) {
-//         var sldie_truoc = $('.active').prev();
-//         if(sldie_truoc.length != 0){
-//             $('.active').addClass('bien-mat-ben-phai').one('webkitAnimationEnd', function (event) {
-//                 $('.bien-mat-ben-phai').removeClass('bien-mat-ben-phai').removeClass('active');
-//             });
-//             sldie_truoc.addClass('active').addClass('di-vao-ben-trai').one('webkitAnimationEnd', function (event) {
-//                 $('.di-vao-ben-trai').removeClass('di-vao-ben-trai');
-//             });
-//         }else {
-//             $('.active').addClass('bien-mat-ben-phai').one('webkitAnimationEnd', function (event) {
-//                 $('.bien-mat-ben-phai').removeClass('bien-mat-ben-phai').removeClass('active');
-//             });
-//             $('.item:last-child').addClass('active').addClass('di-vao-ben-trai').one('webkitAnimationEnd', function (event) {
-//                 $('.di-vao-ben-trai').removeClass('di-vao-ben-trai');
-//             });
-//         }
-//     });
-// });
+
 $(document).ready(function() {
     var currentSlide = 0;
     var totalSlides = $('.slide').length;
 
     function showSlide(slideIndex) {
         $('.slide').hide();
-        $('.slide:eq(' + slideIndex + ')').fadeIn();
+        $('.slide:eq(' + slideIndex + ')').fadeIn(600, 'linear');
     }
 
     $('.right').click(function() {
+        $('.carousel ul li button').removeClass('slick-active');
         currentSlide = (currentSlide + 1) % totalSlides;
+        $('.carousel ul li:nth-child('+(currentSlide + 1)+') button').addClass('slick-active');
         showSlide(currentSlide);
     });
 
     $('.left').click(function() {
+        $('.carousel ul li button').removeClass('slick-active');
         currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+        $('.carousel ul li:nth-child('+(currentSlide + 1)+') button').addClass('slick-active');
         showSlide(currentSlide);
     });
+    // btn phan trang
+    $('.slick').click(function () {
+        clearInterval(interval);
+        $('.carousel ul li button').removeClass('slick-active');
+        $(this).addClass('slick-active');
+        var elementID = $($(this)).attr('id');
+        var index = parseInt(elementID[elementID.length - 1]);
+        showSlide(index);
+    })
 
     // Tự động chuyển slide sau một khoảng thời gian
     function autoSlide() {
+        $('.carousel ul li button').removeClass('slick-active');
         currentSlide = (currentSlide + 1) % totalSlides;
+        $('.carousel ul li:nth-child('+(currentSlide + 1)+') button').addClass('slick-active');
         showSlide(currentSlide);
     }
 
-    var interval = setInterval(autoSlide, 3000);
+    var interval = setInterval(autoSlide, 5000);
 
-    $('.slider').hover(
+    $('.carousel').hover(
         function() {
             clearInterval(interval);
         },
         function() {
-            interval = setInterval(autoSlide, 3000);
+            interval = setInterval(autoSlide, 5000);
         }
     );
 
     showSlide(currentSlide);
 });
+
+/****section 2*******/
+$(document).ready(function() {
+    var initialImage = "../font/img-fsale/item_1.png";
+    var hoverImage = "../font/img-fsale/item_1_hover_sm.png";
+    $(".no-hover-img").hover(
+        function() {
+            $(this).attr("src", hoverImage);
+        },
+        function() {
+            $(this).attr("src", initialImage);
+        }
+    );
+});
+/*fire to-nho*/
+$(document).ready(function() {
+    var isLarge = false;
+
+    function resizeImage() {
+        if (isLarge) {
+            $(".to-nho").css({ width: "30px", height: "30px" });
+        } else {
+            $(".to-nho").css({ width: "50px", height: "50px" });
+        }
+        isLarge = !isLarge; // Đảo ngược trạng thái kích thước
+    }
+    setInterval(resizeImage, 100);
+});
+
+
+
+
+
+
+
