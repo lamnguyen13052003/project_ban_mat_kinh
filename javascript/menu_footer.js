68f0561f19eaeceeade4aac440113dfa13822959
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-24 16:15:37 +0700

    Sửa tên file js cho phù hợp

---
+++ javascript/menu_footer.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("input#search").each(function () {
+        displayPlaceholder($(this));
+    });
+});
+
+function displayPlaceholder(element){
+    if (!element.val()) {
+        var string = "Nhập tên sản phẩm!       ";
+        var val = "";
+        let index = 0;
+        setInterval(function (char) {
+            val += string.charAt(index);
+            element.attr("placeholder", val);
+            index++;
+            if(index === string.length){
+                index = 0;
+                val = "";
+                element.attr("placeholder", "");
+            }
+        }, 150);
+    }
+}
