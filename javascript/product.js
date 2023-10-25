93abf92f8de5807c24ed319525159cfab771ba2f
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-26 06:24:37 +0700

    Chỉnh sửa phần link

--- javascript/product.js
+++ javascript/product.js
@@ -6,17 +6,8 @@ $(document).ready(function () {
     addActionClickForButtonSort();
     addActionFilterMore();
 
-    const storedAccount =  sessionStorage.getItem("account");
-    if (storedAccount != null) {
-        var user = {
-            hình: "../../logo.png",
-            tên: "Nguyễn Đình Lam"
-        }
-        displayMenuAccount(user);
-    }else{
-        $("#menu").find(".login").removeClass("d-none");
-        $("#menu").find(".sign-up").removeClass("d-none");
-    }
+    $("#menu .account a").attr("href", "../../account.html");
+    $("#menu .account a img").attr("src", "../../logo.png");
 });
 
 function addActionClickForButtonFilter() {
@@ -73,19 +64,4 @@ function setRateProduct(){
 function fixSlide(){
     $("#main>div>div.carousel").removeClass("slide").attr("id", "carouselExampleAutoplaying").attr("data-bs-ride", "carousel")
     $("#carouselExampleAutoplaying button").attr("data-bs-target", "#carouselExampleAutoplaying");
-}
-
-function displayMenuAccount(user) {
-    $("#menu").find(".login").addClass("d-none");
-    $("#menu").find(".sign-up").addClass("d-none");
-    $("#menu").find(".sign-up").after(`<div class="account col-lg-3 col-md-2 col-sm-2 border-0 px-lg-0">
-        <a href="../../account.html">
-            <button type="button" class="btn d-flex float-lg-end  me-xl-4 me-lg-2" id="button-account">
-                <div class="avatar p-1 bg-white rounded-circle d-flex align-items-center justify-content-center" style="width: 25px; height: 25px">
-                    <img src="${user.hình}" alt="avatar.png"  class="d-md-line" width="18" height="18">
-                </div>
-                <span class="d-lg-block d-md-none ms-2 overflow-x-hidden d-block" style="max-width: 130px; white-space: nowrap">${user.tên}</span>
-            </button>
-        </a>
-    </div>`);
 }
