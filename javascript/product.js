6357795cafc2681ff7a3c021e6d9869c765a01d4
Lam Nguyen <kiminonawa1305@gmail.com>
2023-10-28 22:42:49 +0700

    Chỉnh sửa lại hiệu ứng animation slide của boostrap thành tự làm. Hoàn thành khung

--- javascript/product.js
+++ javascript/product.js
@@ -5,28 +5,29 @@ $(document).ready(function () {
     removeCheckedFilterAll();
     addActionClickForButtonSort();
     addActionFilterMore();
+    slide();
 });
 
 function addActionClickForButtonFilter() {
-    $("div.filter>ul>li>button").click(function (){
+    $("div.filter>ul>li>button").click(function () {
         changeColorButtonCheck($(this));
     });
 }
 
 function addActionClickForButtonSort() {
-    $("div.sort>ul>li>button").click(function (){
+    $("div.sort>ul>li>button").click(function () {
         changeColorButtonCheck($(this));
     });
 }
 
-function addActionFilterMore(){
-    $("div.filter>ul>li.filter-more").click(function (){
-        if($(this).hasClass("active")){
+function addActionFilterMore() {
+    $("div.filter>ul>li.filter-more").click(function () {
+        if ($(this).hasClass("active")) {
             $(this).parent().children("li.hidden").addClass("d-none");
             $(this).removeClass("active");
             $(this).empty();
             $(this).append('<span class="border-0">Xem thêm <i class="fa-solid fa-arrow-right"></i></span>');
-        }else {
+        } else {
             $(this).parent().children("li.hidden").removeClass("d-none");
             $(this).addClass("active");
             $(this).empty();
@@ -35,21 +36,21 @@ function addActionFilterMore(){
     });
 }
 
-function removeCheckedFilterAll(){
-    $("button#removeCheckedFilterAll").click(function (){
+function removeCheckedFilterAll() {
+    $("button#removeCheckedFilterAll").click(function () {
         $("div.filter>ul>li>button").removeClass("checked");
     });
 }
 
 function changeColorButtonCheck(button) {
-   if(button.hasClass("checked")){
+    if (button.hasClass("checked")) {
         button.removeClass("checked");
-   }else{
-       button.addClass("checked");
-   }
+    } else {
+        button.addClass("checked");
+    }
 }
 
-function setRateProduct(){
+function setRateProduct() {
     var listStar = $("div.onirvapp--shape-container>ul>li");
     for (var star of listStar) {
         if (star.getAttribute("class") === "checked") {
@@ -58,7 +59,20 @@ function setRateProduct(){
     }
 }
 
-function fixSlide(){
-    $("div#main>div>div.carousel").removeClass("slide").attr("id", "carouselExampleAutoplaying").attr("data-bs-ride", "carousel")
-    $("div#carouselExampleAutoplaying button").attr("data-bs-target", "#carouselExampleAutoplaying");
+function fixSlide() {
+    $("#carouselExampleIndicators").attr("id", "carouselIndicators");
+    $("#carouselIndicators").find(".carousel-indicators").removeClass("carousel-indicators").addClass("my-carousel-indicators");
+    $("#carouselIndicators").find(".carousel-inner").removeClass("carousel-inner").addClass("my-carousel-inner");
+    $("#carouselIndicators").find(".carousel-control-prev").removeClass("carousel-control-prev").addClass("my-carousel-control-prev");
+    $("#carouselIndicators").find(".carousel-control-next").removeClass("carousel-control-next").addClass("my-carousel-control-next");
+}
+
+function slide() {
+    $("#carouselIndicators").find(".my-carousel-indicators").find("button").click(function () {
+        const key = $(this).attr("data-bs-slide-to");
+        $("#carouselIndicators").find(".my-carousel-inner").find(".carousel-item").filter(function () {
+            $(this).removeClass("active");
+            $("#carouselIndicators").find(".my-carousel-inner").find(".carousel-item").eq(key).addClass("active");
+        })
+    });
 }
