69463a4bcfb4b66c1c93e2b6f7a1cf91378ce54f
CheBaoHuy <20130277@st.hcmuaf.edu.vn>
2023-12-06 11:51:18 +0700

    haon thanh chuc nang dang nhap

--- src/main/java/model/DAO/UserDAO.java
+++ src/main/java/model/DAO/UserDAO.java
@@ -14,28 +14,35 @@ public class UserDAO {
         return INSTANCE != null ? INSTANCE : new UserDAO();
     }
 
-    public void addUser(User user) {
-    }
-
-    public User getUser(String eamil, String password) {
+    public int addUser(User user) {
         connector = JDBIConnector.get();
-        List<User> users = connector.withHandle(handle ->
-                handle.createQuery("SELECT u.id, u.fullName, u.avatar, u.role FROM users AS u WHERE u.email = ? AND u.password = ? AND u.verify = ? AND u.lock = ?")
-                        .bind(0, eamil)
-                        .bind(1, password)
-                        .bind(2, 1)
-                        .bind(3, 0)
-                        .mapToBean(User.class)
-                        .list()
+        return  connector.withHandle(handle ->
+                handle.createUpdate("INSERT INTO `users` (`id`, `avatar`, `fullName`, `sex`, `birthday`, `email`, `password`, `role`, `verify`, `lock`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+                        .bind(0, user.getAvatar())
+                        .bind(1,user.getFullName())
+                        .bind(2,user.getSex())
+                        .bind(3,user.getBirthDay())
+                        .bind(4,user.getEmail())
+                        .bind(5,user.getPassword())
+                        .bind(6,user.getRole())
+                        .bind(7,0)
+                        .bind(8,0).execute()
+
         );
-
-        return !users.isEmpty() && users.size() == 1 ? users.get(0) : null;
     }
+    public int veryAccountByEmail(String email) {
+        System.out.println(email);
+        connector = JDBIConnector.get();
+        return  connector.withHandle(handle ->
+                handle.createUpdate("Update users set verify = 1 where email = ?")
+                        .bind(0, email).execute()
 
-    public boolean getEmail(String email) {
+        );
+    }
+    public User getUser(String email) {
         connector = JDBIConnector.get();
         List<User> users = connector.withHandle(handle ->
-                handle.createQuery("SELECT u.id FROM users AS u WHERE u.email = ? u.verify = ? AND u.lock = ?")
+                handle.createQuery("SELECT u.id, u.fullName,u.password, u.avatar, u.role FROM users AS u WHERE u.email = ?  AND u.verify = ? AND u.lock = ?")
                         .bind(0, email)
                         .bind(1, 1)
                         .bind(2, 0)
@@ -43,6 +50,20 @@ public class UserDAO {
                         .list()
         );
 
-        return !users.isEmpty();
+        return !users.isEmpty() && users.size() == 1 ? users.get(0) : null;
+    }
+
+    public User getEmail(String email) {
+        connector = JDBIConnector.get();
+        List<User> users = connector.withHandle(handle ->
+                handle.createQuery("SELECT u.id FROM users AS u WHERE u.email = ? AND u.verify = ? AND u.lock = ?")
+                        .bind(0, email)
+                        .bind(1, 0)
+                        .bind(2, 0)
+                        .mapToBean(User.class)
+                        .list()
+        );
+        if(users.size() == 0) return null;
+        return users.get(0);
     }
 }
--- src/main/java/model/service/UserService.java
+++ src/main/java/model/service/UserService.java
@@ -2,6 +2,7 @@ package model.service;
 
 import model.DAO.UserDAO;
 import model.bean.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UserService {
     private static UserService instance;
@@ -21,24 +22,48 @@ public class UserService {
         userDAO = UserDAO.getInstance();
     }
 
-    public boolean canLogin(String eamil, String password) {
-        user = userDAO.getUser(eamil, password);
-        return  user != null;
+    public boolean canLogin(String email, String password) {
+        user = userDAO.getUser(email);
+        if(user == null) return false;
+        System.out.println(user.getPassword() + "_" + password);
+        boolean rs = checkPassword(user.getPassword(),password);
+        return  rs;
+    }
+
+    public boolean findUserByMail(String mail){
+        return userDAO.getEmail(mail) != null;
     }
 
     public User getUser() {
         return user;
     }
 
-    public void signup(User user) {
-        userDAO.addUser(user);
+    public boolean  signup(User user) {
+        user.setPassword(hashPassword(user.getPassword()));
+        int rs =  userDAO.addUser(user);
+        if(rs == 1) return true;
+
+        return false;
+
     }
 
     public void forgetPassword(String email){
 
     }
+    public boolean verifyByEmail(String email){
+        int rs =  userDAO.veryAccountByEmail(email);
+        if(rs == 1) return true;
+        return false;
+    }
+    private String hashPassword(String pass){
+        String hash = BCrypt.hashpw(pass, BCrypt.gensalt(12));
+        return hash;
 
+    }
+    private boolean checkPassword(String hashpass, String pass){
+        return BCrypt.checkpw(pass, hashpass);
+    }
     public boolean containEmail(String email){
-        return userDAO.getEmail(email);
+        return userDAO.getEmail(email) != null;
     }
 }
