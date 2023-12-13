8251e1a0ed771c8ac2162e1e4b5f89eb7ac50b13
LamHongPhong <21130473@st.hcmuaf.edu.vn>
2023-12-13 22:35:41 +0700

    fix tránh null

--- src/main/java/controller/BannerImageController.java
+++ src/main/java/controller/BannerImageController.java
@@ -7,6 +7,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "BannerImageController", value = "/banner-manager")
@@ -25,6 +26,12 @@ public class BannerImageController extends HttpServlet {
         BannerImage urlBannerLoginImages = BannerService.getInstance().getBannerLoginImages(); // banner login
         BannerImage urlBannerSignupImages = BannerService.getInstance().getBannerSignupImages(); // banner Signup
         BannerImage urlBannerLogoImages = BannerService.getInstance().getLogoImages(); // banner logo
+
+        urlBannerImages = urlBannerImages == null? new ArrayList<>() : urlBannerImages;
+        urlBannerPRImages = urlBannerPRImages == null? new BannerImage() : urlBannerPRImages;
+        urlBannerLoginImages = urlBannerLoginImages == null? new BannerImage() : urlBannerLoginImages;
+        urlBannerSignupImages = urlBannerSignupImages == null? new BannerImage() : urlBannerSignupImages;
+        urlBannerLogoImages = urlBannerLogoImages == null? new BannerImage() : urlBannerLogoImages;
 
         request.setAttribute("bannerImages", urlBannerImages); // slide
         request.setAttribute("bannerPRImages", urlBannerPRImages); // banner pr
