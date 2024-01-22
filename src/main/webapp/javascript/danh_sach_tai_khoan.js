24bf463bb4c30d94bd5003545ff1903d2b00662e
CheBaoHuy <20130277@st.hcmuaf.edu.vn>
2024-01-22 21:19:50 +0700

    hoan thanh phan trang quan ly tai khoan

--- src/main/webapp/javascript/danh_sach_tai_khoan.js
+++ src/main/webapp/javascript/danh_sach_tai_khoan.js
@@ -1,95 +1,54 @@
+var pageActive = 1;
+var totalPage = 0;
+
 $(document).ready(function () {
    $("#search-account-id").next().css({"cursor": "pointer"}).click(function () {
+       pageActive = 1
        search_account();
    });
 
     $("#search-account-name").next().css({"cursor": "pointer"}).click(function () {
+        pageActive = 1
         search_account();
 
     });
 
     $("#input-lock-account").change(function () {
+        pageActive = 1
         search_account();
     });
 
     $("#input-account-role").change(function () {
+        pageActive = 1
         search_account();
     });
 });
 
-// function search_account_id(input) {
-//     const list_account = $(".account");
-//     list_account.removeClass("d-none");
-//     const value = input.val();
-//     if (value === "") {
-//         return;
-//     }
-//
-//     list_account.addClass("d-none");
-//     list_account.filter(function () {
-//         var account_id = $(this).attr("data-account-id")[0];
-//         // alert(account_id);
-//         if (account_id.search(value) >= 0) {
-//             $(this).removeClass("d-none");
-//         }
-//     });
-// }
 
-// function search_account_name(input) {
-//     var list_account = $(".account");
-//     list_account.removeClass("d-none");
-//     const value = input.val().toLowerCase();
-//     if (value === "") {
-//         return;
-//     }
-//
-//     list_account.addClass("d-none");
-//     list_account.filter(function () {
-//         var account_name = $(this).attr("data-account-name").join("").toLowerCase();
-//         if (account_name.search(value) >= 0) {
-//             $(this).removeClass("d-none");
-//         }
-//     });
-// }
 
-// function search_lock_account(input) {
-//     var list_account = $(".account");
-//     list_account.removeClass("d-none");
-//     const value = input.val();
-//     if (value === "all") {
-//         return;
-//     }
-//
-//     list_account.addClass("d-none");
-//     list_account.filter(function () {
-//         var lock_account = $(this).attr("data-lock-account");
-//         if (lock_account.search(value) >= 0) {
-//             $(this).removeClass("d-none");
-//         }
-//     });
-// }
-//
-// function search_account_role(input) {
-//     var list_account = $(".account");
-//     list_account.removeClass("d-none");
-//     const value = input.val();
-//     if (value === "all") {
-//         return;
-//     }
-//
-//     list_account.addClass("d-none");
-//     list_account.filter(function () {
-//         var lock_account = $(this).attr("data-account-role");
-//         if (lock_account.search(value) >= 0) {
-//             $(this).removeClass("d-none");
-//         }
-//     });
-// }
+
 function formatCurrencyVND(amount) {
     return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
 }
 
+const handlePage = (page)=>{
+    if(page == -2){
+        pageActive = pageActive -1
+        if(pageActive <= 0)
+            pageActive = pageActive +1
 
+    }else
+    if(page == -1){
+        pageActive = pageActive +1
+        if(pageActive > totalPage)
+            pageActive = pageActive -1
+    }else{
+        pageActive = page
+    }
+
+    search_account()
+
+}
 const search_account = ()=>{
     const id = $("#search-account-id").val();
     const name = $("#search-account-name").val();
@@ -99,7 +58,7 @@ const search_account = ()=>{
         url: '/maven_war/admin/account',
         method: 'GET',
         data: {
-            page: 1,
+            page: pageActive,
             action: 'get',
             name:name,
             role:role,
@@ -109,19 +68,20 @@ const search_account = ()=>{
         },
         dataType: 'json',
         success: function(data) {
+            totalPage  = data.totalPage
             document.querySelector("#count_user").textContent = data.total
             document.querySelector(".amount").value = data.total;
-            let pagi = `<button id="prev" class="d-flex align-items-center justify-content-center"><span
+            let pagi = `<button onclick="handlePage(-2)" id="prev" class="d-flex align-items-center justify-content-center"><span
                             class="material-symbols-outlined">chevron_left</span></button>`
             for (let i = 1; i <= data.totalPage; i++) {
                 pagi += `
-                <button class="d-flex align-items-`c`enter justify-content-center button-number ${i == 1 ? 'active' : ''}"
+                <button onclick="handlePage(${i})" class="d-flex align-items-center justify-content-center button-number ${i == pageActive ? 'active' : ''}"
                             >${i}
                 </button>
                 `
             }
             pagi += `
-            <button id="next" class="d-flex align-items-center justify-content-center"><span
+            <button onclick="handlePage(-1)" id="next" class="d-flex align-items-center justify-content-center"><span
                             class="material-symbols-outlined">chevron_right</span></button>
             `
             document.querySelector(".pagination_account").innerHTML = pagi
