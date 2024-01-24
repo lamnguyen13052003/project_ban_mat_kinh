f3e5d7c3fcceb05e387c1e3264b3039917a1ec2c
LamHongPhong <21130473@st.hcmuaf.edu.vn>
2024-01-24 22:33:45 +0700

    hoàn thành load banner cho trang quen_mat_khau.jsp

---
+++ src/main/java/controller/LoadBannerForForgetPasswordController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.bean.BannerImage;
+import model.service.BannerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LoadBannerForForgetPasswordController", value = "/load-banner-for-forget-password-controller")
+public class LoadBannerForForgetPasswordController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        BannerImage logo = BannerService.getInstance().getBannerByDescription("%banner%logo%");
+        session.setAttribute("logo", logo);
+        request.getRequestDispatcher("quen_mat_khau.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
