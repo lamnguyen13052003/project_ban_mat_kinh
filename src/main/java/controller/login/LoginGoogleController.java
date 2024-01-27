99f896ddad089d35eb60c4bd091538b7ecb3ec97
Lam Nguyen <kiminonawa1305@gmail.com>
2024-01-27 23:54:48 +0700

    Chuyển trang đầu tiên focus khi truy cập vào admin

--- src/main/java/controller/login/LoginGoogleController.java
+++ src/main/java/controller/login/LoginGoogleController.java
@@ -37,7 +37,7 @@ public class LoginGoogleController extends HttpServlet {
 
             request.getSession().setAttribute("user", user);
             if (user.isAdmin()) {
-                response.sendRedirect("admin_pages/quan_ly_tai_khoan.jsp");
+                response.sendRedirect("admin_pages/dashboard.jsp");
             } else {
                 response.sendRedirect("index.jsp");
             }
